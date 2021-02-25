274b9857083fad8027a6823d662db7951438943c
dai-gi <daisuke.u524@gmail.com>
2021-02-26 06:29:14 +0900

    日本に設定変更

--- mysite/settings.py
+++ mysite/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'Asia/Tokyo'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'ja'
 
 USE_I18N = True
 
