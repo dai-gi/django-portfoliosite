7d3ed4e6fefc43a85f4308dca4acf3d15851cfa1
dai-gi <daisuke.u524@gmail.com>
2021-02-26 07:01:10 +0900

    Profileモデルを作成

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Profile(models.Model):
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	content = models.TextField("本文")
+	created = models.DateTimeField("作成日", default=timezone.now)
+
+	def __str__(self):
+		return self.title
