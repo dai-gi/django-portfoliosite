4741d3c80fb399a3eaad738ab6556db2843f870c
dai-gi <daisuke.u524@gmail.com>
2021-02-26 12:49:22 +0900

    トップページを追加

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
+from django.views.generic import View
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        profile_data = Profile.objects.order_by("-id")
+        return render(request, 'app/index.html', {
+            'profile_data': profile_data,
+        })
