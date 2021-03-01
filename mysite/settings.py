024e3a5483230efe6e0254cef2f31de55950e2de
dai-gi <daisuke.u524@gmail.com>
2021-03-02 07:59:53 +0900

    'widget_tweaks'を追加

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'fontawesome-free'
+    'fontawesome-free',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
