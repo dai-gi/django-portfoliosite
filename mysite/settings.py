ebe6b235c374194acda6adeccc19b0c61a9dcd7a
dai-gi <daisuke.u524@gmail.com>
2021-03-12 17:51:28 +0900

    不要アプリケーションを削除

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
-    'fontawesome-free',
     'widget_tweaks',
+    'app',
 ]
 
 MIDDLEWARE = [
