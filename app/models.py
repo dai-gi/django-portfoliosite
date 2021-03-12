a98551d77858de7426d6927473b0f6c127a0bc8c
dai-gi <daisuke.u524@gmail.com>
2021-03-12 20:52:02 +0900

    フィールド追加・削除

--- app/models.py
+++ app/models.py
@@ -1,13 +1,18 @@
-from django.conf import settings
 from django.db import models
-from django.utils import timezone
 
 
 class Profile(models.Model):
-	title = models.CharField('タイトル', max_length=100)
-	image = models.ImageField(upload_to='profile', verbose_name='イメージ画像', null=True, blank=True)
-	content = models.TextField("本文")
-	created = models.DateTimeField("作成日", default=timezone.now)
+	title = models.CharField('タイトル', max_length=100, null=True, blank=True)
+	subtitle = models.CharField('サブタイトル', max_length=100, null=True, blank=True)
+	name = models.CharField('名前', max_length=100)
+	job = models.TextField('仕事')
+	github = models.CharField('github', max_length=100, null=True, blank=True)
+	twitter = models.CharField('twitter', max_length=100, null=True, blank=True)
+	linkedin = models.CharField('linkedin', max_length=100, null=True, blank=True)
+	facebook = models.CharField('facebook', max_length=100, null=True, blank=True)
+	instagram = models.CharField('instagram', max_length=100, null=True, blank=True)
+	topimage = models.ImageField(upload_to='images', verbose_name='トップ画像')
+	subimage = models.ImageField(upload_to='images', verbose_name='サブ画像')
 
 	def __str__(self):
 		return str(self.author)
